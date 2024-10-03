856152e6298465a6a6336d4b990c35899c44d82c
nazzar123 <139446554+nazzar123@users.noreply.github.com>
2024-10-03 16:40:01 +0300

    fix

--- webApp/src/App.js
+++ webApp/src/App.js
@@ -1,28 +1,26 @@
-import { useEffect } from 'react';
-import { Route, Routes } from 'react-router-dom'
 import './App.css';
-import { useTelegram } from './hooks/useTelegram';
-import Header from './components/Header/Header';
+import {useEffect} from "react";
+import {useTelegram} from "./hooks/useTelegram";
+import Header from "./components/Header/Header";
+import {Route, Routes} from 'react-router-dom'
 import Main from './components/Main/Main';
 
 function App() {
-  const {tg, onClose} = useTelegram()
+    const {onToggleButton, tg} = useTelegram();
 
-  useEffect(() => {
-    tg.ready()
-  }, [tg])//удалить tg из зависимостей
+    useEffect(() => {
+        tg.ready();
+    }, [])
 
-  return (
-    <div className="App">
-      <Header/>
-      <Routes>
-        <Route index element={<Main />}></Route>
-        <Route path={'form'} element={<div></div>}></Route>
-      </Routes>
-
-      <button className='footer' onClick={onClose}>Закрыть</button>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <Routes>
+                <Route index element={<Main />}/>
+                <Route path={'form'} element={<div></div>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
--- webApp/src/components/Main/Main.jsx
+++ webApp/src/components/Main/Main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import './LicenseText.css'
+import './Main.css'
 
 const Main = () => {
 
